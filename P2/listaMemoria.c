0f80559b2b4d72f7d86cc3e893de7a4e0347f2f5
Denis Radu <mariodenisradu1705@gmail.com>
2024-11-14 19:48:46 +0100

    Listas Memoria

--- P2/listaMemoria.c
+++ P2/listaMemoria.c
@@ -1,55 +1,115 @@
 #include "listaMemoria.h"
 
 bool mCreateNode(mtPosL *p) {
-    *p = malloc(sizeof(**p));
-    return (*p != NULL);
+   *p = malloc(sizeof(**p));
+   return (*p != NULL);
 }
 
 void mCreateList(mtList *L) {
-    *L = NULL;
+   *L = NULL;
 }
 
 bool mInsertElement(mtItemL d, mtList *L) {
-    mtPosL i, j;
+   mtPosL i, j;
 
-    if (!mCreateNode(&i)) {
-        return false;
-    } else {
-        i->data = d;
-        i->next = NULL;
-        if (*L == NULL) {
-            *L = i;
-        } else {
-            j = *L;
-            while (j->next != NULL) {
-                j = j->next;
-            }
-            j->next = i;
-        }
-        return true;
-    }
+   if (!mCreateNode(&i)) {
+       return false;
+   } else {
+       i->data = d;
+       i->next = NULL;
+       if (*L == NULL) {
+           *L = i;
+       } else {
+           j = *L;
+           while (j->next != NULL) {
+               j = j->next;
+           }
+           j->next = i;
+       }
+       return true;
+   }
 }
 
-mtPosL mFindItem(int df, mtList L) {
-    mtPosL i;
+mtPosL mFindItem(size_t size, char filtro[], char tipo[], mtList L) {
+   mtPosL i;
+   for(i=L; i != NULL; i = i->next) {
+       if ((i->data.size == size) && (strcmp(i->data.type, tipo))) {
+           return i;
+       }
+       if (strcmp(i->data.other, filtro) && (strcmp(i->data.type, tipo))) {
+           return i;
+       }
+       if (strcmp(i->data.other, filtro) && (strcmp(i->data.type, tipo))) {
+           return i;
+       }
+   }
+   return NULL;
 }
 
 void mRemoveElement(mtPosL p, mtList *L) {
-    mtPosL prev = NULL;
-    mtPosL actual = *L; 
+   mtPosL prev = NULL;
+   mtPosL actual = *L;
+
+   if(p == *L) {
+       *L = (*L)->next;
+   } else {
+       while (actual != NULL && actual != p) {
+           prev = actual;
+           actual = actual->next;
+       }
+       if (actual == p) {
+           prev->next = actual->next;
+       }
+   }
+   free(p);
 }
 
 void mDeleteList(mtList *L) {
-    mtPosL temp;
+   mtPosL temp;
 
-    while (*L != NULL) {
-        temp = *L;
-        *L = (*L)->next;
-        free(temp);
-    }
+   while (*L != NULL) {
+       temp = *L;
+       *L = (*L)->next;
+       free(temp);
+   }
 }
 
-void mPrintList (mtList L) {
-    mtPosL i;
+void mPrintList (char tipo[], mtList L) {
+   mtPosL i;
+   char sfecha[64];
+   struct tm *fechaReserva;
+   
+   if(tipo == NULL) {
+       printf("******Lista de bloques asignados para el proceso ");
+       pid();
+   } else {
+       printf("******Lista de bloques asignados %s para el proceso ", tipo);
+       pid();
+   }
 
+   for (i=L; i != NULL; i= i->next) {
+       if(tipo == NULL) {
+           fechaReserva = localtime(&i->data.fecha);
+           strftime(sfecha, sizeof(sfecha),"%b %d %H:%M",fechaReserva);
+           printf("\t%s\t\t%d %s %s", i->data.memAd, i->data.size,sfecha, i->data.type);
+       } else {
+           if (strcmp(tipo, i->data.type) == 0) {
+              fechaReserva = localtime(&i->data.fecha);
+               strftime(sfecha, sizeof(sfecha),"%b %d %H:%M",fechaReserva);
+               printf("\t%s\t\t%d %s %s", i->data.memAd, i->data.size,sfecha, i->data.type);
+           } else if (strcmp(tipo, i->data.type) == 0) {
+               fechaReserva = localtime(&i->data.fecha);
+               strftime(sfecha, sizeof(sfecha),"%b %d %H:%M",fechaReserva);
+               printf("\t%s\t\t%d %s %s (key %s)", i->data.memAd, i->data.size,sfecha, i->data.type,i->data.other);
+           } else if (strcmp(tipo, i->data.type) == 0) {
+               fechaReserva = localtime(&i->data.fecha);
+               strftime(sfecha, sizeof(sfecha),"%b %d %H:%M",fechaReserva);
+               printf("\t%s\t\t%d %s %s (key %s)", i->data.memAd, i->data.size,sfecha, i->data.type, i->data.other);
+           } else {
+               fechaReserva = localtime(&i->data.fecha);
+               strftime(sfecha, sizeof(sfecha),"%b %d %H:%M",fechaReserva);
+               printf("\t%s\t\t%d %s %s (descriptor %s)", i->data.memAd, i->data.size,sfecha, i->data.type, i->data.other);
+           }
+       }
+   }
 }
